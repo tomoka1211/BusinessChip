8ff4a9648811a6ea86efb70c88b203592b61e750
Hirano Tomoka <kamui1211@gmail.com>
2019-06-27 14:42:50 +0900

    指摘項目修正

--- BusinessChip/Views/SettingsViewController.swift
+++ BusinessChip/Views/SettingsViewController.swift
@@ -97,14 +97,4 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         }
     }
     
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
